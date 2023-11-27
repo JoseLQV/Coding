7ed6b3c5d086e6c60d993546021a18c8578551a0
JoseLQV <velezlx1@gmail.com>
2023-11-27 14:38:10 -0400

    Setup

---
+++ Data_Structures/Arrays/Fundamentals.py
@@ -0,0 +1,29 @@
+# [] for the use of arrays
+# empty array = []
+
+# array = [#,#,#,#]:
+numbers = [1,2,3,4]
+words = ["Hello", "World", "Hi"]
+
+# len(array)
+length = len(numbers)
+
+# matrix = [[#], [#], [#]]
+matrix = [numbers,words]
+
+
+# Traverse Array
+
+# # For loops:
+# for number in numbers:
+#     print(number)
+
+# #While loops:
+# index = 0
+# while index < len(numbers):
+#     print(numbers[index])
+#     index += 1
+
+
+
+
