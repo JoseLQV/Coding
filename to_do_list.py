6c756253b0bc104dadbd7872aa7483af2fb70a53
JoseLQV <velezlx1@gmail.com>
2023-12-13 16:05:42 -0400

    To do List:

---
+++ to_do_list.py
@@ -0,0 +1,54 @@
+# To do list:
+
+# Arrays
+#     Sorting arrays
+#     Adding
+#     Removing
+#     Indexing
+#     Dynamic array
+#     2D Array 
+
+# Hashmaps/Dictionaries
+#     Adding , Removing
+#     Sorting
+#     Initialize
+
+# OOP
+#     Interface , Super Class
+#     Class
+#     Private vs Public
+#     Innit (Constructors)
+#     Static vs Nonstatic
+#     Void
+
+# Stack and Queue
+#     Add, Remove, Peek 
+
+# Sorting
+#     Bubble 
+#     Merge
+#     Heap 
+#     Insertion
+#     Quick
+#     Selection
+
+
+
+# Binary Tree
+#     Pre-Order
+#     In-Order
+#     Post-Order
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
