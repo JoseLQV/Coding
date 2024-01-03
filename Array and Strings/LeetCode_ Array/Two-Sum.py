7277bfdc31ee5f6e1be41b750ecfa66dca9e1715
JoseLQV <velezlx1@gmail.com>
2024-01-03 04:25:13 -0400

    Starting attept 2 Two-sum

--- Array and Strings/LeetCode_ Array/Two-Sum.py
+++ Array and Strings/LeetCode_ Array/Two-Sum.py
@@ -29,6 +29,95 @@
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 #------------------------------------------------------------------------------------------
 
+# ATTEMPT 2:
+
+# Description: Array of Int , find the elements index of the sum that = to target
+
+# Conditions:
+    # Array of int
+    # array wont be empty
+    # output any order
+    # given target and array
+    # only one solution
+
+# Exceptions and samples:
+
+    # Input: nums = [2,7,11,15], target = 9
+    # Output: [0,1]
+    # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    # Example 2:
+
+    # Input: nums = [3,2,4], target = 6
+    # Output: [1,2]
+    # Example 3:
+
+    # Input: nums = [3,3], target = 6
+    # Output: [0,1]
+    
+    # Input: nums = [] , target = []
+    #  Output: []
+
+# Brute Force:      
+    #Input: nums = [2,7,11,15], target = 9
+    # Output: [0,1]
+   
+   #Check (none)
+   #Traverse through array
+   
+   
+def solution(nums,target):
+    #Check 
+    if target == None or nums == None:
+        return []
+    
+    
+    map = {}
+    #Traverse
+    for index, number in enumerate(nums):
+        diff = abs(target - number)
+        
+        # Found it
+        if diff in map == True: 
+            return [map[diff], index]
+        
+        #Not Found
+        else:
+            map[index] = number 
+    
+    return []
+            
+            
+
+   
+   
+   
+   
+   
+    
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
+        # ATTEMPT 1
 # Description: given array of int, two nums that sum to target ,  RETURN THE INDEX
 
 # Conditions: 
@@ -110,7 +199,7 @@ class Solution(object):
         return corrected_result
             
             
-# Failed Attepmt Because didnt observe that you have to give the index from the main list not the result
+# Failed Attepmt 1 Because didnt observe that you have to give the index from the main list not the result
 # and changing the values in a sorted array changes its index 
 
   
