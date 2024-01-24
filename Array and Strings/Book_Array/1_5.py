e44a1bd48ba6b0b52556772a3681f99ca23155f7
JoseLQV <velezlx1@gmail.com>
2024-01-24 18:29:47 -0400

    One Away attempt 1 (completed)

--- Array and Strings/Book_Array/1_5.py
+++ Array and Strings/Book_Array/1_5.py
@@ -29,6 +29,8 @@
 # create 3 functions: insert, remove, replace
 
 # function that checks all of the following edits to see if its been edited
+#               5 - 6               -1 (maybe remove), 1(maybe add), 0(naybe replace or none)
+# compare =  len(edited) - len(not)
 # insert:       
 # compare = len(edited) - (len(not)
 # if (compare) <= 0  or (compare) > 1: return false
@@ -45,6 +47,48 @@
 # traverse through both strings check if one edited is changed 
 # if changed once return true 
 # else return False
+
+def testing(string, edited):
+    edits = 0
+    
+    #hashmap:
+    hashmap = {}
+    for n in edited:
+        if n in hashmap:
+            hashmap[n] += 1
+        else:
+            hashmap[n] = 1
+    
+    #traverse:
+    for n in string:
+        if edits >= 2:
+            return False
+        
+        if n in hashmap:
+            #found
+            hashmap[n] -= 1     #reduce it
+            if hashmap[n] == 0: #remove it if 0
+                hashmap.pop(n)
+        else: #not found
+            edits += 1
+            
+    compared = len(string) - len(edited)
+    if len(hashmap) == 1 or compared == 1: return True
+    else: return False
+            
+            
+
+
+        
+    
+    
+    
+        
+
+
+
+        
+    
 
 
 
