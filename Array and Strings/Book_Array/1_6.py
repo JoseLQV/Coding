91b969a5bab656d812a67306f48230915fb659f1
JoseLQV <velezlx1@gmail.com>
2024-01-24 18:43:09 -0400

    Setting up excersice 1_6

---
+++ Array and Strings/Book_Array/1_6.py
@@ -0,0 +1,9 @@
+# String Compression: pg -> 91 , pg ->201
+# Implement a method to perform basic string compression using the counts of repeated characters. For Example, 
+# the string aabccccaaa would become a2b1c5a3.
+# if the "compressed" string would not become smaller than the original string,
+# your method should return the original string. You can assume the string has only 
+# uppercase and lowercase letters(a-z). 
+
+#hints #92, #110
+
