fe544b1b2e4082b61326246daf2a7fdfbf32bcac
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-13 19:10:39 -0400

    setZeroes  : mark first change later

--- Notes/JPmorgan.py
+++ Notes/JPmorgan.py
@@ -7,3 +7,13 @@ observation :
 traverse find lower point and compare until the end, if you find a new lowest then you update it 
 
 """
+
+"""
+setZeroes
+
+in place modification in a matrix
+Make sure to mark first change later, meaning traverse twice for in place problems
+"""
+
+
+
