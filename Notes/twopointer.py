3b5ec35d438f49b4c44449434e188b990aacecac
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-14 12:36:51 -0400

    buy sell  twopointer

---
+++ Notes/twopointer.py
@@ -0,0 +1,9 @@
+""""
+Max Profit: 
+
+since it traverses just from left to right , just update lowest when it finds something lower and change profit accordingly 
+
+observation :
+traverse find lower point and compare until the end, if you find a new lowest then you update it 
+
+"""
