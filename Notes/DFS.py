3c6d6d985ccd295ed7345c542efe8b5be3cfed68
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-15 14:26:01 -0400

    Binary tree paths : DFS while keeping track of all paths

---
+++ Notes/DFS.py
@@ -0,0 +1,12 @@
+
+
+""" 
+Binary Tree Paths:
+
+This is all about the DFS approach and saving the string paths while iteration/recursion
+
+Stack = [(root, str(path))]
+
+this was the way to keep track of the paths 
+
+"""
