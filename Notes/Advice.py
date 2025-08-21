f84b64952c2ae7211aea29305d3d3c4d94966556
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-21 17:07:54 -0400

    DFS traversal specially with infinite boundaries

--- Notes/Advice.py
+++ Notes/Advice.py
@@ -35,6 +35,19 @@ Layer by Layer traversal : Boundaries indexes
             update left
 
 """
+""" 
+1448 Count Good Nodes in Binary tree
+
+Keep track of path max value in a dfs way
+
+"""
+
+""" 
+98. Validate Binary Search Tree
+
+Keep a left and right range starting with -infinity and also +infinity and traverse in a dfs way
+
+"""
 
 
 
