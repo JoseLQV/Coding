7ec5715077012c78b4bf8e941b185402348855bc
Jose Luis Quinones Velez <velezlx1@gmail.com>
2025-08-24 12:02:36 -0400

    Recursion for Max Path 124 DFS

--- Algorithms/Sorting/Sorting.py
+++ Algorithms/Sorting/Sorting.py
@@ -5,3 +5,15 @@ I needed to use Merge sort to be able to solve this problem
 
 Then inside the mergesort do the adding the intervals 
 """
+
+""" 
+124 Binary Tree Maximum Path Sum:
+
+To find max path, update a variable called nonlocal variable while traversing the tree,
+also 3 options
+that entire path 
+left path wins
+right side wins 
+
+All done in recursion 
+"""
